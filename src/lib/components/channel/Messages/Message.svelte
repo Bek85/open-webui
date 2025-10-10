583e4571980e6186d3677a8e2965e63f09df7aa4
Bekzod Akbarov <stx.webdev@gmail.com>
2025-10-10 18:17:56 +0500

    fix build time error in Message.svelte component

--- src/lib/components/channel/Messages/Message.svelte
+++ src/lib/components/channel/Messages/Message.svelte
@@ -282,7 +282,7 @@
 										type={file.type}
 										size={file?.size}
 										small={true}
-                    colorClassName="bg-white dark:bg-[#082556]
+										colorClassName="bg-white dark:bg-[#082556]"
 									/>
 								{/if}
 							</div>
