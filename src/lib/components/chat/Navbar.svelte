022e498875704193b17bd1d9f21bac22d21e9f3f
Bekzod Akbarov <stx.webdev@gmail.com>
2025-10-13 09:17:42 +0500

    feat: add test mode alert badge to chat navbar

--- src/lib/components/chat/Navbar.svelte
+++ src/lib/components/chat/Navbar.svelte
@@ -23,6 +23,7 @@
 	import ShareChatModal from '../chat/ShareChatModal.svelte';
 	import ModelSelector from '../chat/ModelSelector.svelte';
 	import Tooltip from '../common/Tooltip.svelte';
+	import TestModeAlert from '../common/TestModeAlert.svelte';
 	import Menu from '$lib/components/layout/Navbar/Menu.svelte';
 	import UserMenu from '$lib/components/layout/Sidebar/UserMenu.svelte';
 	import AdjustmentsHorizontal from '../icons/AdjustmentsHorizontal.svelte';
@@ -107,7 +108,8 @@
 					{/if}
 				</div>
 
-				<div class="self-start flex flex-none items-center text-gray-600 dark:text-gray-400">
+				<div class="self-start flex flex-none items-center gap-2 text-gray-600 dark:text-gray-400">
+					<TestModeAlert />
 					<!-- <div class="md:hidden flex self-center w-[1px] h-5 mx-2 bg-gray-300 dark:bg-stone-700" /> -->
 
 					{#if $user?.role === 'user' ? ($user?.permissions?.chat?.temporary ?? true) && !($user?.permissions?.chat?.temporary_enforced ?? false) : true}
---
+++ src/lib/components/common/TestModeAlert.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+
+	const i18n = getContext('i18n');
+</script>
+
+<div class="flex items-center">
+	<div
+		class="px-3 py-0.5 rounded-lg bg-amber-100 dark:bg-amber-900 border border-amber-300 dark:border-amber-700"
+	>
+		<span class="text-xs font-medium text-amber-900 dark:text-amber-100 whitespace-nowrap">
+			Tizim test rejimida ishlamoqda
+		</span>
+	</div>
+</div>
